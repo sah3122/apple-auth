e2ea102ba80e156b4bfbd1e07b2fd3d0b71b6a5e
dongchul <sah3122@naver.com>
2019-09-17 23:50:55 +0900

    ADD JWT authenticate

--- src/main/java/me/toy/appleauth/provider/JwtAuthenticationProvider.java
+++ src/main/java/me/toy/appleauth/provider/JwtAuthenticationProvider.java
@@ -1,10 +1,14 @@
 package me.toy.appleauth.provider;
 
 import me.toy.appleauth.services.JwtUserDetailsService;
+import me.toy.appleauth.token.JwtAuthenticationToken;
+import me.toy.appleauth.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,13 +20,25 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private JwtUserDetailsService userDetailsService;
 
+    // Token 검증 하는 부분
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        if(authentication.getCredentials() == null) {
+            throw new BadCredentialsException("Invalid Token");
+        }
+
+        String token = authentication.getCredentials().toString();
+
+        if(JWTUtil.verify(token)) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(token);
+            return new JwtAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Invalid Token");
+        }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return JwtAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
